6f8d5cff3103cc001045818f019973592d95ac62
Janis <janisdr2016@gmail.com>
2026-01-23 17:30:49 +0100

    Now Working more on the pages!

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,38 @@
-import { createGraph , mockData} from "./graph/graph";
+import { createGraph, mockData } from "./graph/graph";
 import '../styling/custom.scss';
 
-createGraph('#chart-revenue', mockData);
-createGraph('#chart-users', mockData);
+const initNavigation = () => {
+    const toggleButtons = document.querySelectorAll<HTMLAnchorElement>('.section-toggle');
+    const sections = document.querySelectorAll<HTMLElement>('.content-section');
 
-/*
-window.addEventListener("DOMContentLoaded", () => {
-    createGraph('#chart-revenue', mockData);
-    createGraph('#chart-users', mockData);
+    toggleButtons.forEach(button => {
+        button.addEventListener('click', (e: MouseEvent) => {
+            // disable ahref default behaviour moving to '#'
+            e.preventDefault();
+
+            const targetId = button.getAttribute('data-target');
+            if (!targetId) return;
+
+            // hide all sections
+            sections.forEach(section => {
+                section.style.display = 'none';
+            });
+
+            // enable target section
+            const targetSection = document.getElementById(targetId);
+            if (targetSection) {
+                targetSection.style.display = 'block';
+            }
+
+            // Set active to current button
+            toggleButtons.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+        });
+    });
+
+    toggleButtons[0].click();
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    initNavigation();
 });
-*/
